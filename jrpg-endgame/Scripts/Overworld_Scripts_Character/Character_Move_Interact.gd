2b2c7c82ee5c3799ffe9324d82c113b991f76655
Tvgo2 <krazygluekid@gmail.com>
2025-05-20 18:21:32 -0500

    Random_Shit_Middle_Of

---
+++ jrpg-endgame/Scripts/Overworld_Scripts_Character/Character_Move_Interact.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+var Collision_X: int
+var Collision_Y: int
+var Move_Distance: Vector2
+var Move_Direction: Vector2
+var Movement: bool = false
+
+
+func _physics_process(delta: float) -> void:
+	pass
+
+func _on_collison_box_ready() -> void:
+	Collision_X = get_child(0).shape.size.x
+	Collision_Y = get_child(0).shape.size.y
+	Move_Distance = Vector2(Collision_X,Collision_Y)
+	print(Move_Distance)
+
+func _on_move_input_timer_timeout() -> void:
+	pass
